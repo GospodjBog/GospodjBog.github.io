b69b1c5a8e4597ace3b7a95e4a6911b97ae180fa
Gospod Bog <100020618+GospodjBog@users.noreply.github.com>
2023-04-12 15:27:20 +0300

    Оформление

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const dropBtn = document.getElementById("drop-btn");
 const dropList = document.getElementById("drop-list");
+const switchTheme = document.getElementById("switch-1");
 
-dropBtn.addEventListener("click", (e) => {
+dropBtn.addEventListener("click", () => {
   dropList.classList.toggle("hidden");
 });
 window.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") return;
   if (!dropList.classList.contains("hidden")) dropList.classList.add("hidden");
+});
+
+switchTheme.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
 });
